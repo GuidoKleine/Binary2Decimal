22300dfddd02840bc3596067438359140d437fcf
Guido Kleine <33725136+GuidoKleine@users.noreply.github.com>
2019-07-02 15:35:59 +0200

    add the mathmatical function

--- app.js
+++ app.js
@@ -5,40 +5,51 @@ alert = document.getElementById("alert");
 btn.addEventListener("click", checkInputData);
 
 function checkInputData() {
-    // get number from inputfiled
+    // get number from inputfield
     binaryNumber = document.getElementById("inputField").value;
-
+    var temp = 0
     //check if number consist of either 0 and 1
     for (let index = 0; index < binaryNumber.length; index++) {
         if (isNaN(binaryNumber)) {
-            document.getElementById("alert").style.visibility = "visible"
-            document.getElementById("outputField").value = " ";
+            showWarning()
             return;
         } else if (binaryNumber.charAt(index) == 0 || binaryNumber.charAt(index) == 1) {
-            var temp = ""
             temp += binaryNumber.charAt(index)
-            ;console.log(temp);
-            
-            convertBin2Dec(temp)
         } else {
-            document.getElementById("alert").style.visibility = "visible"
-            document.getElementById("outputField").value = " ";
+            showWarning()
             return;
         }
     }
-        //convertBin2Dec(temp)
+    convertBin2Dec(temp)
+}
+
+function showWarning() {
+    document.getElementById("alert").style.visibility = "visible"
+    document.getElementById("outputField").value = " ";
+    return;
 }
 
 function convertBin2Dec(binaryToConvert) {
-    // convert binary number to decimal number
-    // om binar uit te rekenen moet je 2 tot de macht "lengte" gebruiken dus 10 = (1*(2^1) + 0*(2^0))
     document.getElementById("alert").style.visibility = "hidden"
-    let temp;
-
+    // convert binary number to decimal number
+    // om binary uit te rekenen moet je 2 tot de macht "lengte" gebruiken dus 10 = (1*(2^1) + 0*(2^0)) 
+    reverseLength(binaryToConvert)
+    let result = 0
     for (let index = 0; index < binaryToConvert.length; index++) {
-        temp = binaryToConvert.charAt(index);
-        document.getElementById("outputField").value = temp;
-    }
+        base = 2
+        exponent = reverseNumber[index]
+        result += binaryToConvert[index] * (base**exponent)     
+    } 
+    document.getElementById("outputField").value = result;
+}
 
-    //document.getElementById("outputField").value = temp
+// gets the reverse length of binaryToConvert variable
+function reverseLength(binaryLength){
+        var num = binaryLength.length-1;
+    for (let index = binaryLength.length-2; index > -1; index--) {
+        let n = index.toString();
+        num += n
+        reverseNumber = num
+    }
+    return reverseNumber;
 }
